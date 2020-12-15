cbc2bbb1dee94e3ccf177c4381ab3011095ca897
Hamza HERBOU <hamza95herbou@gmail.com>
2020-12-15 11:55:43 +0100

    added files

---
+++ Assets/Demo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine ;
+
+public class Demo : MonoBehaviour {
+
+   void Start () {
+      Debug.Log ("message 1") ;
+      this.Wait (2f, () => {
+         Debug.Log ("message 2") ;
+         this.Wait (2f, () => {
+            Debug.Log ("message 3") ;
+            this.Wait (2f, () => {
+               Debug.Log ("The last message") ;
+               Camera.main.backgroundColor = Color.magenta ;
+            }) ;
+         }) ;
+      }) ;
+   }
+
+}
---
+++ Assets/Extensions/WaitExtension.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine ;
+using UnityEngine.Events ;
+using System.Collections ;
+
+public static class WaitExtension {
+   public static void Wait (this MonoBehaviour mono, float delay, UnityAction action) {
+      mono.StartCoroutine (ExecuteAction (delay, action)) ;
+   }
+
+   private static IEnumerator ExecuteAction (float delay, UnityAction action) {
+      yield return new WaitForSecondsRealtime (delay) ;
+      action.Invoke () ;
+      yield break ;
+   }
+}
